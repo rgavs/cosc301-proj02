f63e6629527a9f121efdf7b5151410045e997fc6
Ryan Gavigan <gavigan.ryan@gmail.com>
2015-09-29 19:43:53 -0400

    Segmentation fault re cmds var

--- main.c
+++ main.c
@@ -12,11 +12,42 @@
 #include <signal.h>
 
 int main(int argc, char **argv) {
-    char buff[1024];
-    printf("shell# ");
-    fgets(buff,1024,stdin);
-    // switch (/* expression */) {
-    //     case /* value */:
-    // }
+    char * buff = malloc(1024*sizeof(char *));
+    char * prompt = "shell# ";
+    printf("%s",prompt);
+    fflush(stdout);
+    int num_cmds = 1;
+    while(fgets(buff,1024,stdin) != NULL){
+        strtok(buff,"\n");
+        printf("|%s|\n",buff);
+        for(int i = 0; i < strlen(buff) - 1; i++){
+            if(buff[i] == '#'){
+                buff[i] = '\0';
+                break;
+            }
+            if(buff[i] == '&' && buff[i + 1 == '&'])
+                num_cmds ++;
+        }
+        char ** cmds[num_cmds];
+        if(num_cmds == 1){
+            strcpy(*cmds[0],buff);
+        }
+        else{
+            int n = 0;
+            int tmp = 0;
+            while(n < num_cmds){
+                for(int j = 0; j < strlen(buff) - 1; j++){
+                    if(buff[j] == '&' && buff[j + 1] == '&'){
+                        strncpy(*cmds[n],&buff[tmp],j - tmp); // copies from previous word (or start) until j
+                        tmp = j;
+                        strncpy(*cmds[j - n],"\0",1);
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < num_cmds; i++)
+            printf("%s\n",*cmds[i]);
+    }
+    free(buff);
     return 0;
 }
